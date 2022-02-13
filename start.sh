3e8a9263b453d278cf55edca14618725b16509aa
Britt <me@britt.us>
2022-02-12 20:35:49 -0500

    update nogui

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 shopt -s extglob; set +H
 cp -ar data-temp/*/!(server.properties|ops.json|banned-ips.json|banned-players.json|whitelist.json|usercache.json) /data/
 jarfile=$(cd /data && ls|grep forge-*.jar)
-cd /data && java -server "-Xms${INIT_MEM}" "-Xmx${MAX_MEM}" "-XX:+UnlockExperimentalVMOptions" "-XX:+UseCGroupMemoryLimitForHeap" "-XshowSettings:vm" "-Dfml.queryResult=confirm" -jar $jarfile nogui
+cd /data && java -server "-Xms${INIT_MEM}" "-Xmx${MAX_MEM}" "-XX:+UnlockExperimentalVMOptions" "-XX:+UseCGroupMemoryLimitForHeap" "-XshowSettings:vm" "-Dfml.queryResult=confirm" -jar $jarfile --nogui 
